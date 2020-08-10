629dfbb6d63001880d0d89d55e028da4901da3dc
mr.mazurmaciej@gmail.com <mr.mazurmaciej@gmail.com>
2020-08-10 21:55:45 +0100

    Updates

---
+++ precache-manifest.854eede928af28541929c57626c99e73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b00d9632dfa616219f8055ec95ef1d",
+    "url": "/uptime/index.html"
+  },
+  {
+    "revision": "2757503b7f04e738acbd",
+    "url": "/uptime/static/css/main.1d079b49.chunk.css"
+  },
+  {
+    "revision": "f174cc6e56e15a0121ba",
+    "url": "/uptime/static/js/2.ebf5eb24.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/uptime/static/js/2.ebf5eb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2757503b7f04e738acbd",
+    "url": "/uptime/static/js/main.49b70043.chunk.js"
+  },
+  {
+    "revision": "e1cb57b80855165f7798",
+    "url": "/uptime/static/js/runtime-main.becfd1b5.js"
+  },
+  {
+    "revision": "cf804e296d5c1907cfd574c2be1cbc78",
+    "url": "/uptime/static/media/preloader.cf804e29.gif"
+  }
+]);
